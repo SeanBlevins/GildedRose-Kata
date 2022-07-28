0c5c708be709863b720dc6574465c98fa6215779
sean Blevins <sean.blevins256@gmail.com>
2022-07-28 14:25:42 +0200

    Change GildedRoseTest to test some basic app level assumptions

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,14 +4,27 @@ import com.gildedrose.model.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void gildedRoseInput() {
+       assertThrows(IllegalArgumentException.class, () -> new GildedRose(null));
+    }
+
+    @Test
+    void gildedRoseEmptyItemArray() {
+        Item[] items = new Item[] {};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items.length);
+    }
+    @Test
+    void gildedRoseSingleItem() {
+        Item[] items = new Item[] { new Item("random item", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items.length);
     }
 }
