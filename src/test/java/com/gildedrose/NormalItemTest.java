f1b122a89ba732b4ea556365eddaf7b438ca280d
sean Blevins <sean.blevins256@gmail.com>
2022-07-28 14:08:58 +0200

    A few more tests for normal items

--- src/test/java/com/gildedrose/NormalItemTest.java
+++ src/test/java/com/gildedrose/NormalItemTest.java
@@ -33,6 +33,24 @@ public class NormalItemTest {
     }
 
     @Test
+    void randomItemTest_1Day_passedSellInDate() {
+        Item[] items = new Item[] { new Item("random item", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
+    }
+
+    @Test
+    void randomItemTest_1Day_passedSellInDate_qualityMinimum() {
+        Item[] items = new Item[] { new Item("random item", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
     void nullItemNameTest_1Day() {
         Item[] items = new Item[] { new Item(null, 10, 10) };
         GildedRose app = new GildedRose(items);
